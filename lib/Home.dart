faa166a564a0b0396ce143d30b1ab2be9e677226
masudrr60 <masudrr60@gmail.com>
2025-07-21 01:05:37 +0600

    wrap and layout

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,41 +1,74 @@
 import 'package:flutter/material.dart';
 class Home extends StatefulWidget {
   const Home({super.key});
-
   @override
   State<Home> createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
-  @override
-  
-  
-  Widget build(BuildContext context) {
+  final List<String> categories = [
+    'Rahat','Rah','fafa','dafda',  'Rahat','Rah','fafa','dafda',
+    'Rahat','Rah','fafa','dafda',    'Rahat','Rah','fafa','dafda'
+  ];
 
-    double screenwidth = MediaQuery.of(context).size.width;
-    double screenheight = MediaQuery.of(context).size.height;
-    
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
-        title:Text("Media Query")),
-      body: Center(
-        child: Container(
-          height: screenheight * 5,
-          width: screenwidth *1,
-          decoration: BoxDecoration(color: Colors.cyanAccent),
-          child: Center(
-            child: Text("Rahat fuck you",
-              style: TextStyle(
-                  fontSize: screenwidth *0.09,
-                  color: Colors.red,
-                fontWeight: FontWeight.bold,),
+        title:Text("Wrap example")),
+      body:Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: categories.map((singleItem){
+                return Chip(label: Text(singleItem),
+                  backgroundColor:Colors.red,
+                );
+            }).toList(),
             ),
           ),
-        ),
+          SizedBox(height: 10,),
 
-      ),
+//****************************** layoutBuilder***************************
+
+           LayoutBuilder(
+            builder: (context, con) {
+              if (con.maxWidth > 600) {
+                return Center(
+                  child: Text("This is tablet layout"),
+                ); // Center
+              } else {
+                return Center(
+                  child: Text("This is mobile layout",style:TextStyle(fontSize:30),),
+                ); // Center
+              }
+            }),
+
+          SizedBox(height: 20,),
+
+
+          Expanded(child: Padding(padding: const EdgeInsets.all(8.0),
+            child: GridView.count(
+              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 5,
+              children:List.generate(
+                100,
+                  (index) =>Container(
+                    color: Colors.cyanAccent,
+                  )),
+            ),
+          ),),
+          SizedBox(
+            height: 10,
+          ),
+
+        ],
+      )
     );
   }
 }
